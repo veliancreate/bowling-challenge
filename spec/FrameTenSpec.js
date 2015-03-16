624d65b104e08eef578ca6693fb4a23e49d3440c
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-16 20:28:32 +0000

    refactored fed frame object instances

--- spec/FrameTenSpec.js
+++ spec/FrameTenSpec.js
@@ -11,58 +11,58 @@ describe ('FrameTen', function(){
   });
 
   it('if roll one is a strike the roll one score is 10', function(){
-    frame.getRollOne(10)
+    frame.getRoll(10)
     expect(frame.rollOneScore).toEqual(10)
   });
    
   it('if roll one is not a strike, it knows that it will have to add up the roll score', function(){
-    frame.getRollOne(5)
+    frame.getRoll(5)
     expect(frame.rollOneScore).toEqual(5)
   }); 
 
   it('knows that if roll one was not a strike, then the pins remaining should be less than the roll', function(){
-    frame.getRollOne(5)
+    frame.getRoll(5)
     expect(frame.pinsRemaining).toEqual(5)
   });
 
   it('knows that if roll one was not a strike, then it has to add up roll two scores', function(){
-    frame.getRollOne(5);
-    frame.getRollTwo(5);
+    frame.getRoll(5);
+    frame.getRoll(5);
     expect(frame.rollOneScore).toEqual(5)
     expect(frame.rollTwoScore).toEqual(5)
   });
 
   it('knows that if there was a strike on roll two, that roll three can commence', function(){
-    frame.getRollOne(5);
-    frame.getRollTwo(10);
+    frame.getRoll(5);
+    frame.getRoll(10);
     expect(frame.isRollThree()).toEqual(true);  
   });
 
   it('knows that if there was a spare on roll two, that roll three can commence', function(){
-    frame.getRollOne(5);
-    frame.getRollTwo(5);
+    frame.getRoll(5);
+    frame.getRoll(5);
     expect(frame.isRollThree()).toEqual(true);  
   });
 
   it('knows that if there was NOT a spare on roll two, that roll three cannot commence', function(){
-    frame.getRollOne(3);
-    frame.getRollTwo(4);
+    frame.getRoll(3);
+    frame.getRoll(4);
     expect(frame.isRollThree()).toEqual(false);  
   });
 
   it('can correctly score rolls', function(){
-    frame.getRollOne(3)
-    frame.getRollTwo(3)
-    frame.getRollThree(3)
+    frame.getRoll(3)
+    frame.getRoll(3)
+    frame.getRoll(3)
     expect(frame.rollOneScore).toEqual(3)
     expect(frame.rollTwoScore).toEqual(3)
     expect(frame.rollThreeScore).toEqual(0)
   });
 
   it('can correctly score rolls', function(){
-    frame.getRollOne(10)
-    frame.getRollTwo(10)
-    frame.getRollThree(10)
+    frame.getRoll(10)
+    frame.getRoll(10)
+    frame.getRoll(10)
     expect(frame.rollOneScore).toEqual(10)
     expect(frame.rollTwoScore).toEqual(10)
     expect(frame.rollThreeScore).toEqual(10)
@@ -70,9 +70,9 @@ describe ('FrameTen', function(){
 
 
   it('can correctly score rolls', function(){
-    frame.getRollOne(5)
-    frame.getRollTwo(5)
-    frame.getRollThree(10)
+    frame.getRoll(5)
+    frame.getRoll(5)
+    frame.getRoll(10)
     expect(frame.rollOneScore).toEqual(5)
     expect(frame.rollTwoScore).toEqual(5)
     expect(frame.rollThreeScore).toEqual(10)
